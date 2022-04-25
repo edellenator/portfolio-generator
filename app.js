65b39dca68960342887162d6cda9763d4145db63
Erik Dell <edellenator@gmail.com>
2022-04-24 19:58:41 -0700

    app generates html file

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [uName, github] = profileDataArgs;
+const fs = require('fs');
+const generatePage = require('./src/page-template')
+
+// const printProfileData = profileDataArr => {
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+
+// }
+// printProfileData(profileDataArgs);
 
 
-const printProfileData = profileDataArr => {
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+fs.writeFile('index.html', generatePage(uName, github), err =>{
+    if (err) throw err;
 
-}
-printProfileData(profileDataArgs);
-
+    console.log('Portfolio complete! Check out index.html to see the output');
+});
