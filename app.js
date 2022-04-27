59e6938f016d03703f1ac7b24f59fd18b7ccc50f
Erik Dell <edellenator@gmail.com>
2022-04-26 19:59:35 -0700

    inquirer

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [uName, github] = profileDataArgs;
-const fs = require('fs');
-const generatePage = require('./src/page-template')
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template')
 
-// const printProfileData = profileDataArr => {
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+  
+// const pageHTML = generatePage(uName, github);
 
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-
-// }
-// printProfileData(profileDataArgs);
 
 
+// fs.writeFile('./index.html', pageHTML, err =>{
+//     if (err) throw err;
 
-fs.writeFile('index.html', generatePage(uName, github), err =>{
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output');
+// });
